2e3c791c20e110283c293aced620221aba2d05e3
johntran038 <johntran038@gmail.com>
2025-01-30 01:49:43 -0600

    replaced newsletter

--- src/pages/general-components/Footer.jsx
+++ src/pages/general-components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FooterSection from './footer-components/FooterSection';
-import NewsLetterSection from './footer-components/NewsLetterSection';
+import SocialLinks from './footer-components/SocialLinks';
 
 const Footer = () => {
 
@@ -13,22 +13,26 @@ const Footer = () => {
                         <div className="flex justify-center md:justify-start items-center col-span-1">
                             <img src="images/temp/naturalflo-temp-logo.png" alt="NaturalFlo Logo" className='h-[10rem] w-[15rem] object-contain' />
                         </div>
-                        <FooterSection className='col-span-1'
-                            title='Shop'
-                            contents={[
-                                { title: 'Featured Products', link: '/#featured', type: 'navigate' },
-                                { title: 'All Products', link: '/products', type: 'link' },
-                            ]}
-                        />
-                        <FooterSection className='col-span-1'
-                            title='Follow Us'
-                            contents={[
-                                { title: 'Social Link 1', link: '', type: 'new tab' },
-                                { title: 'Social Link 2', link: '', type: 'new tab' },
-                            ]}
-                        />
-                        <div className='col-span-1 md:col-span-3 lg:col-span-1 w-full'>
-                            <NewsLetterSection />
+                        <div className='col-span-1 pr-10 lg:pr-5 text-nowrap'>
+                            <FooterSection
+                                title='Shop'
+                                contents={[
+                                    { title: 'Featured Products', link: '/#featured', type: 'navigate' },
+                                    { title: 'All Products', link: '/products', type: 'link' },
+                                ]}
+                            />
+                        </div>
+                        <div className='col-span-1 pr-10 lg:pr-5 text-nowrap'>
+                            <FooterSection
+                                title='More Ways to Connect'
+                                contents={[
+                                    { title: 'Link 1', link: '', type: 'new tab' },
+                                    { title: 'Link 2', link: '', type: 'new tab' },
+                                ]}
+                            />
+                        </div>
+                        <div className='col-span-1 pr-10 lg:pr-5 md:col-span-3 lg:col-span-1 w-full text-nowrap'>
+                            <SocialLinks />
                         </div>
                     </div>
 
