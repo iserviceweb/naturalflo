e025f71a3cce50e5ec2eb02e589aaea6fea2e397
John Tran <johntran038@gmail.com>
2025-07-21 16:35:48 -0500

    started fixing links

---
+++ utils/Links.js
@@ -0,0 +1,12 @@
+
+const Links = {
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    instagram: "https://www.instagram.com/naturalflo___/?igsh=OTl4Zm1hNnh6cGlo&utm_source=qr",
+    etsy: "https://www.etsy.com/people/6awjsqitqd5cqdj3",
+    linkedin: "https://linkedin.com",
+    // : "",
+    // : "",
+}
+
+export default Links
